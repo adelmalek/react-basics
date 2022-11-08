73aa9cd43403c89d111d44042a5fc16f981bcb59
Adel <gerencser.adel@gmail.com>
2022-11-08 19:12:58 +0100

    input

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    console.log('constructor');
     super();
 
     this.state = {
       monsters: [],
     }
+
+    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('3');
+    console.log('componentdidmount');
+
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(res => res.json())
       .then(users => this.setState({monsters:users}))
   }
 
   render() {
-    console.log("2");
+    console.log('render');
+
     return (
       <div className="App">
+      <input className='search-box' type='search' placeholder='search monster' onChange={(e) => console.log(e.target.value)} />
         {this.state.monsters.map(monster => <div key={monster.id}><h2>{monster.name}</h2></div>)}
       </div>
     );
